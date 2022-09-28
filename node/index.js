d77e43bc320077ace77b839aef80335d12a1af24
César O. Araújo <oaraujocesar@gmail.com>
2022-09-28 01:22:27 -0300

    initial commit: playing with docker

---
+++ node/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const port = 3333;
+
+const config = {
+  host: "db",
+  user: "root",
+  password: "root",
+  database: "nodedb",
+};
+
+const mysql = require("mysql");
+const connection = mysql.createConnection(config);
+
+const sql = `INSERT INTO people(name) VALUES('César')`;
+
+connection.query(sql);
+connection.end();
+
+app.get("/", (req, res) => {
+  res.send("<h1>Full Cycle</h1>");
+});
+
+app.listen(port, () => {
+  console.log("Rodando na porta " + port);
+});
